1d62ddc60a8faccb1ccb2ec116797d9f108613b7
Em <158059167+Laglue-em@users.noreply.github.com>
2026-04-15 15:45:47 +0200

    Create t-shirt.py

---
+++ t-shirt.py
@@ -0,0 +1,6 @@
+size=input("What size do you want:").strip()
+message=input("What message on your shirt:").strip()
+def make_shirt(size,message):
+    print(f"I want a shirt size {size} with this message '{message}'.")
+    
+make_shirt(size,message)
